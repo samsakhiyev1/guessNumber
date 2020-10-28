0667fbddd1fea410975195d66451bcd6557fd43a
Sam <72391438+samsakhiyev1@users.noreply.github.com>
2020-10-28 15:51:51 +0200

    Create game.py

---
+++ game.py
@@ -0,0 +1,21 @@
+import random
+
+number = random.randint(0, 101)
+
+while True:
+    answer = input("Enter the number: ")
+    if not answer or answer == "exit":
+        break
+
+    if not answer.isdigit():
+        print("Enter integer number")
+        continue
+    user_answer = int(answer)
+
+    if user_answer > number:
+        print ("Number is lower")
+    elif user_answer < number:
+        print("Number is higher")
+    else:
+        print("BINGO!!!")
+        break
